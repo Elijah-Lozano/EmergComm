13b87c60a3a07f99f1fee29515f237888220c753
elijah-lozano <elijahlozano@hotmail.com>
2021-03-27 14:39:06 -0500

    added diseases

--- Model/Candidate.js
+++ Model/Candidate.js
@@ -7,6 +7,18 @@ class Candidate {
     livesWithRespIllnessPatient = false
     worksInHighRiskSetting = false
 
+    isBIPOC = false
+
+    hasDiseases = {
+        lungDisease: false,
+        heartDisease: false,
+        hyperTension: false,
+        highBloodPressure: false,
+        diabetes: false
+    }
+
+
+
     constructor(age, occupation, residence) {
 
         this.age = age
@@ -47,6 +59,25 @@ class Candidate {
     }
     get getWorksInHighRiskSetting(){
         return this.worksInHighRiskSetting
+    }
+
+    setIsBIPOC(isBIPOC){
+        this.isBIPOC = isBIPOC
+    }
+    get getIsBIPOC(){
+        return this.isBIPOC
+    }
+
+    setDiseases(hasLD, hasHD, hasHT, hasHBP, hasD){
+        this.hasDiseases["lungDisease"] = hasLD
+        this.hasDiseases["heartDisease"] = hasHD
+        this.hasDiseases["hyperTension"] = hasHT
+        this.hasDiseases["highBloodPressure"] = hasHBP
+        this.hasDiseases["diabetes"] = hasD
+    }
+
+    get getDiseases(){
+        return this.hasDiseases
     }
 
 }
